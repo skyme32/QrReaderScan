0d56637eb5efcd370cea1a03d43bee3ea254ed27
skyme32 <marcosmejiasg@gmail.com>
2020-02-03 12:43:52 +0100

    add type

--- app/src/main/java/com/example/qrreaderscan/CameraActivity.kt
+++ app/src/main/java/com/example/qrreaderscan/CameraActivity.kt
@@ -1,17 +1,14 @@
 package com.example.qrreaderscan
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
-import android.webkit.URLUtil
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.vision.CameraSource
@@ -29,6 +26,7 @@ class CameraActivity : AppCompatActivity() {
     private val MY_PERMISSIONS_REQUEST_CAMERA = 1
     private var token = ""
     private var tokenanterior = ""
+    private val LOG_TAG = "Barcode Scanner API"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,13 +42,16 @@ class CameraActivity : AppCompatActivity() {
 
         // creo el detector qr
         val barcodeDetector = BarcodeDetector.Builder(this)
-            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .setBarcodeFormats(Barcode.DATA_MATRIX or Barcode.QR_CODE)
             .build()
 
         // creo la camara
         cameraSource = CameraSource.Builder(this, barcodeDetector)
-            .setRequestedPreviewSize(320, 320)
+            //.setRequestedPreviewSize(320, 320)
             .setAutoFocusEnabled(true) //you should add this feature
+            .setFacing(CameraSource.CAMERA_FACING_BACK)
+            .setRequestedPreviewSize(1600, 1024)
+            .setRequestedFps(15.0f)
             .build()
 
         // listener de ciclo de vida de la camara
@@ -74,7 +75,7 @@ class CameraActivity : AppCompatActivity() {
                     try {
                         cameraSource!!.start(cameraView!!.holder)
                     } catch (ie: IOException) {
-                        Log.e("CAMERA SOURCE", ie.message)
+                        //Log.e("CAMERA SOURCE", ie.message)
                     }
 
                 }
@@ -99,31 +100,62 @@ class CameraActivity : AppCompatActivity() {
 
                     // obtenemos el token
                     token = barcodes.valueAt(0).displayValue.toString()
-
-
                     // verificamos que el token anterior no se igual al actual
                     // esto es util para evitar multiples llamadas empleando el mismo token
+
+                        val code = barcodes.valueAt(0)
+                        val type = barcodes.valueAt(0).valueFormat
+
+                        //Log.i(LOG_TAG, type.toString() + " - " + Barcode.CONTACT_INFO)
+
+                        when (type) {
+                            Barcode.CONTACT_INFO -> {
+                                //Log.i(LOG_TAG, code.contactInfo.name.first)
+                                //Log.i(LOG_TAG, code.contactInfo.name.last)
+
+
+                                for (index in 0 until code.contactInfo.phones.size) {
+                                    when (code.contactInfo.phones[index].type) {
+                                        Barcode.Phone.HOME -> Log.i(LOG_TAG, "HOME: " + code.contactInfo.phones[index].number)
+                                        Barcode.Phone.MOBILE -> Log.i(LOG_TAG, "MOBILE: " + code.contactInfo.phones[index].number)
+                                        Barcode.Phone.CONTENTS_FILE_DESCRIPTOR -> Log.i(LOG_TAG, "CONTENTS_FILE_DESCRIPTOR: " + code.contactInfo.phones[index].number)
+                                        Barcode.Phone.PARCELABLE_WRITE_RETURN_VALUE -> Log.i(LOG_TAG, "PARCELABLE_WRITE_RETURN_VALUE: " + code.contactInfo.phones[index].number)
+                                        Barcode.Phone.FAX -> Log.i(LOG_TAG, "FAX: " + code.contactInfo.phones[index].number)
+                                        Barcode.Phone.WORK -> Log.i(LOG_TAG, "WORK: " + code.contactInfo.phones[index].number)
+                                        Barcode.Phone.UNKNOWN -> Log.i(LOG_TAG, "UNKNOWN: " + code.contactInfo.phones[index].number)
+                                    }
+
+                                }
+
+
+
+                                //Log.i(LOG_TAG, code.contactInfo.phones[0].number)
+                            }
+                            Barcode.EMAIL -> Log.i(LOG_TAG, code.email.address)
+                            Barcode.ISBN -> Log.i(LOG_TAG, code.rawValue)
+                            Barcode.PHONE -> Log.i(LOG_TAG, code.phone.number)
+                            Barcode.PRODUCT -> Log.i(LOG_TAG, code.rawValue)
+                            Barcode.SMS -> Log.i(LOG_TAG, code.sms.message)
+                            Barcode.TEXT -> Log.i(LOG_TAG, code.rawValue)
+                            Barcode.URL -> Log.i(LOG_TAG, "url: " + code.url.url)
+                            Barcode.WIFI -> Log.i(LOG_TAG, code.wifi.ssid)
+                            Barcode.GEO -> Log.i(LOG_TAG, code.geoPoint.lat.toString() + ":" + code.geoPoint.lng)
+                            Barcode.CALENDAR_EVENT -> Log.i(LOG_TAG, code.calendarEvent.description)
+                            Barcode.DRIVER_LICENSE -> Log.i(LOG_TAG, code.driverLicense.licenseNumber)
+                            else -> Log.i(LOG_TAG, code.rawValue)
+                        }
+
+
                     if (token != tokenanterior) {
 
                         // guardamos el ultimo token proceado
                         tokenanterior = token
-                        Log.i("token", token)
 
-                        /**
-                        if (URLUtil.isValidUrl(token)) {
-                            // si es una URL valida abre el navegador
-                            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(token))
-                            startActivity(browserIntent)
-                        } else {
-                            // comparte en otras apps
-                            val shareIntent = Intent()
-                            shareIntent.action = Intent.ACTION_SEND
-                            shareIntent.putExtra(Intent.EXTRA_TEXT, token)
-                            shareIntent.type = "text/plain"
-                            startActivity(shareIntent)
-                        }**/
+                        val code = barcodes.valueAt(0)
 
                         textView2?.text = token
+
+
 
                         Thread(object : Runnable {
                             override fun run() {
