d6b6332a2eb123ef105fe747d264d0cb7462fa0d
skyme32 <marcosmejiasg@gmail.com>
2020-02-04 23:48:52 +0100

    Add the recyclerView to the view

--- app/src/main/java/com/example/qrreaderscan/CameraActivity.kt
+++ app/src/main/java/com/example/qrreaderscan/CameraActivity.kt
@@ -4,13 +4,12 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.example.qrreaderscan.bean.Code
 import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
 import com.google.android.gms.vision.barcode.Barcode
@@ -27,6 +26,7 @@ class CameraActivity : AppCompatActivity() {
     private var token = ""
     private var tokenanterior = ""
     private val LOG_TAG = "Barcode Scanner API"
+    private var shortAnimationDuration: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,111 +106,25 @@ class CameraActivity : AppCompatActivity() {
         barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
             override fun release() {}
 
-
             override fun receiveDetections(detections: Detector.Detections<Barcode>) {
                 val barcodes = detections.getDetectedItems()
 
                 if (barcodes.size() > 0) {
 
-                    // obtenemos el token
-                    token = barcodes.valueAt(0).displayValue.toString()
-                    // verificamos que el token anterior no se igual al actual
-                    // esto es util para evitar multiples llamadas empleando el mismo token
-
                     val code = barcodes.valueAt(0)
-                    val type = barcodes.valueAt(0).valueFormat
+                    var codeAt = Code(code)
 
-                    //Log.i(LOG_TAG, type.toString() + " - " + Barcode.CONTACT_INFO)
-
-                    when (type) {
-                        Barcode.CONTACT_INFO -> {
-                            //Log.i(LOG_TAG, code.contactInfo.name.first)
-                            //Log.i(LOG_TAG, code.contactInfo.name.last)
-
-
-                            for (index in 0 until code.contactInfo.phones.size) {
-                                when (code.contactInfo.phones[index].type) {
-                                    Barcode.Phone.HOME -> Log.i(
-                                        LOG_TAG,
-                                        "HOME: " + code.contactInfo.phones[index].number
-                                    )
-                                    Barcode.Phone.MOBILE -> Log.i(
-                                        LOG_TAG,
-                                        "MOBILE: " + code.contactInfo.phones[index].number
-                                    )
-                                    Barcode.Phone.CONTENTS_FILE_DESCRIPTOR -> Log.i(
-                                        LOG_TAG,
-                                        "CONTENTS_FILE_DESCRIPTOR: " + code.contactInfo.phones[index].number
-                                    )
-                                    Barcode.Phone.PARCELABLE_WRITE_RETURN_VALUE -> Log.i(
-                                        LOG_TAG,
-                                        "PARCELABLE_WRITE_RETURN_VALUE: " + code.contactInfo.phones[index].number
-                                    )
-                                    Barcode.Phone.FAX -> Log.i(
-                                        LOG_TAG,
-                                        "FAX: " + code.contactInfo.phones[index].number
-                                    )
-                                    Barcode.Phone.WORK -> Log.i(
-                                        LOG_TAG,
-                                        "WORK: " + code.contactInfo.phones[index].number
-                                    )
-                                    Barcode.Phone.UNKNOWN -> Log.i(
-                                        LOG_TAG,
-                                        "UNKNOWN: " + code.contactInfo.phones[index].number
-                                    )
-                                }
-
-                            }
-
-
-                            //Log.i(LOG_TAG, code.contactInfo.phones[0].number)
-                        }
-
-                        Barcode.EMAIL -> Log.i(LOG_TAG, code.email.address)
-                        Barcode.ISBN -> Log.i(LOG_TAG, code.rawValue)
-                        Barcode.PHONE -> Log.i(LOG_TAG, code.phone.number)
-                        Barcode.PRODUCT -> Log.i(LOG_TAG, code.rawValue)
-                        Barcode.SMS -> Log.i(LOG_TAG, code.sms.message)
-                        Barcode.TEXT -> Log.i(LOG_TAG, code.rawValue)
-                        Barcode.URL -> Log.i(LOG_TAG, "url: " + code.url.url)
-                        Barcode.WIFI -> Log.i(LOG_TAG, code.wifi.ssid)
-                        Barcode.GEO -> Log.i(
-                            LOG_TAG,
-                            code.geoPoint.lat.toString() + ":" + code.geoPoint.lng
-                        )
-                        Barcode.CALENDAR_EVENT -> Log.i(LOG_TAG, code.calendarEvent.description)
-                        Barcode.DRIVER_LICENSE -> Log.i(LOG_TAG, code.driverLicense.licenseNumber)
-                        else -> Log.i(LOG_TAG, code.rawValue)
+                    textView2?.text = code.displayValue
+                    textView2?.apply {
+                        alpha = 0f
+                        visibility = View.VISIBLE
+                        animate()
+                            .alpha(1f)
+                            .setDuration(shortAnimationDuration.toLong())
+                            .setListener(null)
                     }
 
 
-                    if (token != tokenanterior) {
-
-                        // guardamos el ultimo token proceado
-                        tokenanterior = token
-                        val code = barcodes.valueAt(0)
-
-                        textView2?.text = token
-
-
-
-                        Thread(object : Runnable {
-                            override fun run() {
-                                try {
-                                    synchronized(this) {
-                                        // limpiamos el token
-                                        tokenanterior = ""
-                                    }
-                                } catch (e: InterruptedException) {
-                                    // TODO Auto-generated catch block
-                                    Log.e("Error", "Waiting didnt work!!")
-                                    e.printStackTrace()
-                                }
-
-                            }
-                        }).start()
-
-                    }
                 }
             }
         })
--- app/src/main/java/com/example/qrreaderscan/MainActivity.kt
+++ app/src/main/java/com/example/qrreaderscan/MainActivity.kt
@@ -2,40 +2,175 @@ package com.example.qrreaderscan
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.qrreaderscan.bean.Code
+import com.example.qrreaderscan.factory.AdapterCustom
+import com.example.qrreaderscan.listener.ClickListener
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var adapterCustom: AdapterCustom? = null
+    var listRecycle: RecyclerView? = null
+
+
+    companion object {
+        var codes = ArrayList<Code>()
+        var sortCodes: List<Code> = emptyList()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
-            val intent = Intent(this, CameraActivity::class.java)
-            startActivity(intent)
+        //Only for testing
+        texting()
+
+        //
+        listRecycle = findViewById(R.id.qrRecyclerList)
+        listRecycle?.setHasFixedSize(true)
+        listRecycle?.layoutManager = LinearLayoutManager(this)
+        //sortData(true)
+
+        codes.sort
+
+        // Implement onClick and OnLongClick to the Elements of the list
+        var click = createAdapterOnclick()
+        adapterCustom = AdapterCustom(codes, click)
+        listRecycle?.adapter = adapterCustom
+
+        // Simulated the infoarmation
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.swiprefresh)
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            var code1 = Code().apply {
+                img = R.drawable.ic_wifi
+                title = "Asto_es_bueno"
+                date = "28/01/2020"
+                type = 9
+            }
+            codes.add(code1)
+            adapterCustom?.notifyDataSetChanged()
         }
+
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(view.context, CameraActivity::class.java)
+            //startActivity(intent)
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+
+    private fun createAdapterOnclick(): ClickListener {
+        // Implement onclick to the Elements of the list, only one click
+        return object : ClickListener {
+            override fun onClick(itemView: View, index: Int) {
+                Toast.makeText(
+                    applicationContext,
+                    index.toString() + " - " + codes[index].title,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+    }
+
+    fun texting() {
+        var code1 = Code().apply {
+            img = R.drawable.ic_wifi
+            title = "Esto_es_bueno"
+            date = "28/01/2020"
+            type = 9
+        }
+
+        var code2 = Code().apply {
+            img = R.drawable.ic_event
+            title = "Es tu dia"
+            date = "28/01/2020"
+            type = 11
+        }
+
+        var code3 = Code().apply {
+            img = R.drawable.ic_account_circle
+            title = "Skyme32"
+            date = "28/01/2020"
+            type = 1
+        }
+
+        var code4 = Code().apply {
+            img = R.drawable.ic_unknown_late
+            title = "Texto normal y corriente"
+            date = "28/01/2020"
+            type = 14
+        }
+
+
+        codes.add(code1)
+        codes?.add(code2)
+        codes?.add(code3)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code4)
+        codes?.add(code2)
+        codes?.add(code1)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code2)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code2)
+        codes?.add(code1)
+        codes?.add(code1)
+        codes?.add(code2)
+        codes?.add(code3)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code4)
+        codes?.add(code2)
+        codes?.add(code1)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code2)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code2)
+        codes?.add(code1)
+        codes?.add(code1)
+        codes?.add(code2)
+        codes?.add(code3)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code4)
+        codes?.add(code2)
+        codes?.add(code1)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code2)
+        codes?.add(code4)
+        codes?.add(code3)
+        codes?.add(code2)
+        codes?.add(code1)
     }
 }
